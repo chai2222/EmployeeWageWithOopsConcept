4ba34029a39b2f867813825c3b8d780724207dcd
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-03 15:33:49 +0530

    EmployeePartTime

--- EmployeeWageComputationProblem/EmployeeAttendance.cs
+++ EmployeeWageComputationProblem/EmployeeAttendance.cs
@@ -6,19 +6,30 @@ namespace EmployeeWageComputationProblem
     {
         public  void checkAttendance()
         {
-            int is_Present = 1;
+            int is_Full_Time = 1;
             int is_Wage_Per_Hour = 20;
+
+            int is_Part_Time = 2;
+
             int emp_Wage = 0;
             int emp_Hour = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
 
-            if (empCheck == is_Present)
+            if (empCheck == is_Full_Time)
             {
                 Console.WriteLine("Employee Is Present");
                 emp_Hour = 8;
             }
+
+            else if (empCheck == is_Part_Time)
+            {
+                Console.WriteLine("Employee Is Part Time");
+                emp_Hour = 4;
+            }
+
+
             else
             {
                 Console.WriteLine("Employee Is Absent");
