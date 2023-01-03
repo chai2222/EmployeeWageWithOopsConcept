bab2f2e59903235de4efdbc533f35e4a911f64fc
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-03 15:40:33 +0530

    EmolpyeeMonthWage

--- EmployeeWageComputationProblem/EmployeeAttendance.cs
+++ EmployeeWageComputationProblem/EmployeeAttendance.cs
@@ -10,30 +10,37 @@ namespace EmployeeWageComputationProblem
             const int is_Wage_Per_Hour = 20;
 
             const int is_Part_Time = 2;
+            const int num_Of_Working_days = 20;
 
             int emp_Wage = 0;
             int emp_Hour = 0;
+            int total_Emp_Wage = 0;
 
             Random random = new Random();
             int empCheck = random.Next(3);
 
-            switch (empCheck)
+            for (int i = 0; i < num_Of_Working_days; i++)
             {
-                case is_Full_Time:
-                    Console.WriteLine("Employee Is Present & Full Time");
-                    emp_Hour = 8;
-                    break;
-                case is_Part_Time:
-                    Console.WriteLine("Employee Is Part Time");
-                    emp_Hour = 4;
-                    break;
-                case 0:
-                    Console.WriteLine("Employee Is Absent");
-                    break;
+                switch (empCheck)
+                {
+                    case is_Full_Time:
+                        Console.WriteLine("Employee Is Present & Full Time");
+                        emp_Hour = 8;
+                        break;
+                    case is_Part_Time:
+                        Console.WriteLine("Employee Is Part Time");
+                        emp_Hour = 4;
+                        break;
+                    case 0:
+                        Console.WriteLine("Employee Is Absent");
+                        break;
+                }
+                
+                emp_Wage = emp_Hour * is_Wage_Per_Hour;
+                total_Emp_Wage += emp_Wage;
+                Console.WriteLine("Employee Wage :" + emp_Wage);
             }
-
-            emp_Wage = emp_Hour * is_Wage_Per_Hour;
-            Console.WriteLine("Employee Wage :" + emp_Wage);
+            Console.WriteLine("Total Employee Wage is :" + total_Emp_Wage);
         }
     }
 }
