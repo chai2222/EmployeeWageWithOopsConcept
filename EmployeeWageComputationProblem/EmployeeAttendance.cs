c2b0661b344032087ca4e53e4bed96128fc53c14
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-03 15:30:44 +0530

    EmployeeWageCheck

--- EmployeeWageComputationProblem/EmployeeAttendance.cs
+++ EmployeeWageComputationProblem/EmployeeAttendance.cs
@@ -7,6 +7,9 @@ namespace EmployeeWageComputationProblem
         public  void checkAttendance()
         {
             int is_Present = 1;
+            int is_Wage_Per_Hour = 20;
+            int emp_Wage = 0;
+            int emp_Hour = 0;
 
             Random random = new Random();
             int empCheck = random.Next(2);
@@ -14,11 +17,14 @@ namespace EmployeeWageComputationProblem
             if (empCheck == is_Present)
             {
                 Console.WriteLine("Employee Is Present");
+                emp_Hour = 8;
             }
             else
             {
                 Console.WriteLine("Employee Is Absent");
             }
+            emp_Wage = emp_Hour * is_Wage_Per_Hour;
+            Console.WriteLine("Employee Wage :" + emp_Wage);
         }
     }
 }
